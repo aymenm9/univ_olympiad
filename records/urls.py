341b8e6e7f400dc4d4a854d3b186ee82ea2b4144
aymen merad <ay28mene@gmail.com>
2025-05-05 22:49:36 +0100

    fix urls

--- records/urls.py
+++ records/urls.py
@@ -6,12 +6,12 @@ urlpatterns=[
     path('death/', views.DeathRecordView.as_view(), name='death-record'),
     path('birth/<str:birth_number>/', views.BirthRecordDetailView.as_view(), name='birth-record-detail'),
     path('death/<str:death_number>/', views.DeathRecordDetailView.as_view(), name='death-record-detail'),
-    path('birth_certificate_pdf/<str:birth_number>/', views.BirthCertificateView.as_view(), name='generate-birth-pdf'),
-    path('death_certificate_pdf/<str:death_number>/', views.DeathCertificateView.as_view(), name='generate-death-pdf'),
+    path('birth_certificate_pdf/<str:birth_number>/', views.BirthCertificatePDF.as_view(), name='generate-birth-pdf'),
+    path('death_certificate_pdf/<str:death_number>/', views.DeathCertificatePDf.as_view(), name='generate-death-pdf'),
     path('birth_certificate/', views.BirthCertificateView.as_view(), name='birth-certificate'),
     path('death_certificate/', views.DeathCertificateView.as_view(), name='death-certificate'),
-    path('birth_certificate/<str:birth_number>/', views.BirthCertificateView.as_view(), name='birth-certificate'),
-    path('death_certificate/<str:death_number>/', views.DeathCertificateView.as_view(), name='death-certificate'),
+    path('birth_certificate/<str:birth_number>/', views.BirthCertificateDetailView.as_view(), name='birth-certificate'),
+    path('death_certificate/<str:death_number>/', views.DeathCertificateDetailView.as_view(), name='death-certificate'),
     path('statistics/', views.StatisticView.as_view(), name='statistics'),
 
 
