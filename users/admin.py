6afd406118505ca34798669766376389f06e8ac0
aymen merad <ay28mene@gmail.com>
2025-05-07 23:04:31 +0100

    add court

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import UserInfo, Hospital, DSP, APC
+from .models import UserInfo, Hospital, DSP, APC, Court
 # Register your models here.
 
 admin.site.register(UserInfo)
 admin.site.register(Hospital)
 admin.site.register(DSP)
 admin.site.register(APC)
+admin.site.register(Court)
 
 
 
